73258d73665aeb5d288f8e9686526e8208dd7ddc
Damian <damianprakash07@gmail.com>
2023-08-08 23:18:07 +0530

    Added shopping items and styling(css)

--- src/products.js
+++ src/products.js
@@ -1,8 +1,59 @@
-export const PRODUCST = [
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+
+export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPHONE",
+    productName: "Image1",
     price: 999.0,
-    productImage: "123",
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Image2",
+    price: 999.0,
+    productImage: product2,
+  },
+  {
+    id: 1,
+    productName: "Image3",
+    price: 999.0,
+    productImage: product3,
+  },
+  {
+    id: 1,
+    productName: "Image4",
+    price: 999.0,
+    productImage: product4,
+  },
+  {
+    id: 1,
+    productName: "Image5",
+    price: 999.0,
+    productImage: product5,
+  },
+  {
+    id: 1,
+    productName: "Image6",
+    price: 999.0,
+    productImage: product6,
+  },
+  {
+    id: 1,
+    productName: "Image7",
+    price: 999.0,
+    productImage: product7,
+  },
+  {
+    id: 1,
+    productName: "Image8",
+    price: 999.0,
+    productImage: product8,
   },
 ];
