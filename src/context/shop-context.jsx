967fa30115bf9006856bda8fe0735906667d2a63
Damian <damianprakash07@gmail.com>
2023-08-11 23:41:44 +0530

    contect provider implementation

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { createContext } from "react";
+
+export const ShopContext = createContext(null);
 
 export const ShopContextProvider = () => {
   return <div>shoop-context</div>;
