09f0accb4367c801a37af02fc824c6c01e90e49e
Damian <damianprakash07@gmail.com>
2023-08-12 23:53:25 +0530

    carts

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,7 +1,16 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "./products";
 
 export const ShopContext = createContext(null);
 
-export const ShopContextProvider = () => {
-  return <div>shoop-context</div>;
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.lenght; i++) {
+    cart[i] = 0;
+  }
+};
+
+export const ShopContextProvider = (props) => {
+  const [CartItems, setCartItems] = useState();
+  return <ShopContext.Provider>shoop-context</ShopContext.Provider>;
 };
